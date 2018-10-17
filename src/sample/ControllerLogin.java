0427456b666e3535954a7ef12b463204914de021
ilanpogr <ilanpogreb01@gmail.com>
2018-10-17 00:51:44 -0700

    login_view.fxml added login function. controllerLogin at early stages

--- src/sample/ControllerLogin.java
+++ src/sample/ControllerLogin.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class ControllerLogin {
+    public TextField pass;
+    public TextField id;
+    User user;
+
+    //TODO - add function: after login.. all user's data saved in "user".
+
+    public void login(ActionEvent actionEvent) {
+        String password = pass.getText();
+        String userName = id.getText();
+
+    }
+
 }
