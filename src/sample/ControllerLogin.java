821b74bfd81b34867f299ac6dce16cf67d8b225c
ilanpogr <ilanpogreb01@gmail.com>
2018-10-17 04:04:19 -0700

    ControllerLogin - login function created with to error options first: additional error stage second: (right now in use) text label if error

--- src/sample/ControllerLogin.java
+++ src/sample/ControllerLogin.java
@@ -2,23 +2,47 @@ package sample;
 
 import db.DatabaseManager;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class ControllerLogin {
     public TextField pass;
     public TextField id;
+    public Button login_bttn;
+    public Label error_lbl;
     User user;
 
-    //TODO - add function: after login.. all user's data saved in "user".
+    private void errorWindow(String title, String header, String content){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
 
     public void login(ActionEvent actionEvent) {
         String password = pass.getText();
         String userName = id.getText();
         user = DatabaseManager.selectUser(userName, password);
         if(user == null){
-
+//            errorWindow("ERROR", "Invalid Connection", "Incorrect Username or Password...");
+            error_lbl.setText("Incorrect Username or Password");
+        } else {
+            Stage stage = (Stage)login_bttn.getScene().getWindow();
+            stage.close();
         }
+        error_lbl.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> ov, String t, String t1) {
+                System.out.println(" ");
+            }
+        });
 
     }
 
