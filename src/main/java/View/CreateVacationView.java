9266a79907f61fb7e5729b69ec3603f5c5d9ed11
ilanpogr <ilanpogreb01@gmail.com>
2018-11-24 14:25:00 +0200

    createVacation fxml created with controller (view) NOT implemented

---
+++ src/main/java/View/CreateVacationView.java
@@ -0,0 +1,28 @@
+package View;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class CreateVacationView {
+    public Label errorMessage_d_date;
+    public Label errorMessage_r_date;
+    public Label errorMessage_d_time;
+    public Label errorMessage_r_time;
+    public TextField price;
+    public TextField flightNumber;
+    public TextField airline;
+    public TextField destination;
+    public TextField numberOfTickets;
+    public ChoiceBox vacation_type_choice;
+    public ChoiceBox accommodation_type_choice;
+    public TextField depart_time;
+    public TextField return_time;
+
+    public void handleCancelButtonAction(ActionEvent actionEvent) {
+    }
+
+    public void handleSubmitButtonAction(ActionEvent actionEvent) {
+    }
+}
