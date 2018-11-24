2532e40605b28be0995c199c360a3a1b369fc7fd
ilanpogr <ilanpogreb01@gmail.com>
2018-11-24 15:06:46 +0200

    commented all additional info -> for faster testing

--- src/main/java/View/CreateVacationView.java
+++ src/main/java/View/CreateVacationView.java
@@ -4,22 +4,22 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 
 public class CreateVacationView {
-    public Label errorMessage_d_date;
-    public Label errorMessage_r_date;
-    public Label errorMessage_d_time;
-    public Label errorMessage_r_time;
-    public TextField price;
-    public TextField flightNumber;
-    public TextField airline;
+//    public Label errorMessage_d_date;
+//    public Label errorMessage_r_date;
+//    public Label errorMessage_d_time;
+//    public Label errorMessage_r_time;
+//    public TextField price;
+//    public TextField flightNumber;
+//    public TextField airline;
     public TextField destination;
-    public TextField numberOfTickets;
-    public ChoiceBox vacation_type_choice;
-    public ChoiceBox accommodation_type_choice;
-    public TextField depart_time;
-    public TextField return_time;
-    public DatePicker return_datePicker;
-    public DatePicker depart_datePicker;
-    public TextArea freeText;
+//    public TextField numberOfTickets;
+//    public ChoiceBox vacation_type_choice;
+//    public ChoiceBox accommodation_type_choice;
+//    public TextField depart_time;
+//    public TextField return_time;
+//    public DatePicker return_datePicker;
+//    public DatePicker depart_datePicker;
+//    public TextArea freeText;
 
     public void handleCancelButtonAction(ActionEvent actionEvent) {
     }
