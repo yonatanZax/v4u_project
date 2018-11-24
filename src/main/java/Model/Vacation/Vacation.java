938ac673b9c3fa2ac63480ad76e8850ec55e2f2f
Shawn <shawn@post.bgu.ac.il>
2018-11-24 10:58:46 +0200

    Vacation(Table?).java

--- src/main/java/Model/Vacation/Vacation.java
+++ src/main/java/Model/Vacation/Vacation.java
@@ -2,16 +2,21 @@ package Model.Vacation;
 
 public class Vacation {
 
-    private String vacationKey;
     // Todo - add more fields ( also getters and setters )
+    private String vacationKey;
+    private String sellerKey;
+    private String origin;
+    private String destination;
+    private boolean visible;
+    private int timeStamp;
 
-
-    public Vacation(){
-        this.vacationKey = null;
-    }
-
-    public Vacation(String vKey){
-        this.vacationKey = vKey;
+    public Vacation(String vacationKey, String sellerKey, String origin, String destination, boolean visible, int timeStamp) {
+        this.vacationKey = vacationKey;
+        this.sellerKey = sellerKey;
+        this.origin = origin;
+        this.destination = destination;
+        this.visible = visible;
+        this.timeStamp = timeStamp;
     }
 
     public String getVacationKey() {
@@ -20,5 +25,45 @@ public class Vacation {
 
     public void setVacationKey(String vacationKey) {
         this.vacationKey = vacationKey;
+    }
+
+    public String getSellerKey() {
+        return sellerKey;
+    }
+
+    public void setSellerKey(String sellerKey) {
+        this.sellerKey = sellerKey;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    public int getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(int timeStamp) {
+        this.timeStamp = timeStamp;
     }
 }
--- src/main/java/db/Tables/VacationTable.java
+++ src/main/java/db/Tables/VacationTable.java
@@ -16,33 +16,37 @@ public class VacationTable extends ATableManager<Vacation> {
 
     private static VacationTable ourInstance;
     public final String COLUMN_VACATIONTABLE_KEY = "key";
+    public static final String COLUMN_VACATIONTABLE_SELLERKEY = "sellerKey";
+    private final String FOREIGNKEY_SELLERKEY = "(" + COLUMN_VACATIONTABLE_SELLERKEY + ") references userInfo(key)";
+    public static final String COLUMN_VACATIONTABLE_VISIBLE = "visible";
+    public static final String COLUMN_VACATIONTABLE_DESTINATION = "destination";
+    public static final String COLUMN_VACATIONTABLE_ORIGIN = "origin";
+    public static final String COLUMN_VACATIONTABLE_TIMESTAMP = "timestamp";
 
     // Todo - add more columns
 
 
-
     // Singleton
     public static VacationTable getInstance() {
-        if(ourInstance == null) {
+        if (ourInstance == null) {
             ourInstance = new VacationTable();
         }
         return ourInstance;
     }
 
     private VacationTable() {
-        super(DBManager.getInstance(),"vacationInfo");
+        super(DBManager.getInstance(), "vacationInfo");
         createTable();
     }
 
     @Override
     public DBResult createTable() {
-        // Todo - add parameters
         String[] primaryKeys = {COLUMN_VACATIONTABLE_KEY};
-        String[] foreignKeys = {};
-        String[] stringFields = {};
-        String[] intFields = {};
+        String[] foreignKeys = {FOREIGNKEY_SELLERKEY};
+        String[] stringFields = {COLUMN_VACATIONTABLE_SELLERKEY, COLUMN_VACATIONTABLE_ORIGIN, COLUMN_VACATIONTABLE_DESTINATION};
+        String[] intFields = {COLUMN_VACATIONTABLE_TIMESTAMP, COLUMN_VACATIONTABLE_VISIBLE};
         String[] doubleFields = {};
-        return super.createTable(primaryKeys, foreignKeys,stringFields,intFields,doubleFields);
+        return super.createTable(primaryKeys, foreignKeys, stringFields, intFields, doubleFields);
     }
 
 
@@ -53,33 +57,41 @@ public class VacationTable extends ATableManager<Vacation> {
     }
 
 
-
     @Override
     protected PreparedStatement getInsertPreparedStatement(Vacation object, Connection connection) {
         /*
-        * vacationKey
-        * sellerKey
-        * price
-        * flightNo
-        * flightCompany
-        * origin
-        * dest
-        * fromDate
-        * toDate
-        * numOfTickets
-        * flightBack
-        *
-        *
-        * */
-
-
-        String sql = "INSERT INTO " + TABLE_NAME + "(" + COLUMN_VACATIONTABLE_KEY + "," + ") VALUES(?,)";
+         * vacationKey
+         * sellerKey
+         * price
+         * flightNo
+         * flightCompany
+         * origin
+         * dest
+         * fromDate
+         * toDate
+         * numOfTickets
+         * flightBack
+         *
+         *
+         * */
+        String sql = "INSERT INTO " + TABLE_NAME + "("
+                + COLUMN_VACATIONTABLE_KEY
+                + "," + COLUMN_VACATIONTABLE_SELLERKEY
+                + "," + COLUMN_VACATIONTABLE_ORIGIN
+                + "," + COLUMN_VACATIONTABLE_DESTINATION
+                + "," + COLUMN_VACATIONTABLE_TIMESTAMP
+                + "," + COLUMN_VACATIONTABLE_VISIBLE
+                + ") VALUES(?,?,?,?,?,?)";
         PreparedStatement pstmt = null;
         if (connection != null) {
             try {
                 pstmt = connection.prepareStatement(sql);
                 pstmt.setString(1, object.getVacationKey());
-                // Todo - add more parameters here
+                pstmt.setString(2, object.getSellerKey());
+                pstmt.setString(3, object.getOrigin());
+                pstmt.setString(4, object.getDestination());
+                pstmt.setObject(5, object.isVisible()? 1 : 0);
+                pstmt.setObject(6, object.getTimeStamp());
                 return pstmt;
             } catch (SQLException e) {
                 System.out.println(e.getMessage());
@@ -92,8 +104,6 @@ public class VacationTable extends ATableManager<Vacation> {
         }
         return null;
     }
-
-
 
 
 }
