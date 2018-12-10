8fe366efee46bf6b69b39b8285d0c591a0c5f08b
ilanpogr <ilanpogreb01@gmail.com>
2018-12-10 22:55:54 +0200

    RequestTable reading not updated table (table is updated)

--- src/main/java/MainPackage/Main.java
+++ src/main/java/MainPackage/Main.java
@@ -1,11 +1,14 @@
 package MainPackage;
 
+import Model.Purchase.Purchase;
 import Model.Request.Request;
 import Model.Request.RequestModel;
 import Model.User.User;
 import Model.User.UserModel;
 import Model.Vacation.Vacation;
 import Model.Vacation.VacationModel;
+import PaypalPackage.PaypalPayment;
+import PaypalPackage.PaypalTable;
 import db.Tables.*;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -20,27 +23,35 @@ public class Main extends Application {
         if (! directory.exists())
             directory.mkdir();
 
-        UserTable.getInstance().createTable();
-        VacationTable.getInstance().createTable();
-        PurchaseTable.getInstance().createTable();
-        RequestTable.getInstance().createTable();
-        PaymentTable.getInstance().createTable();
+        PaypalTable paypalTable = PaypalTable.getInstance();
+        PaypalPayment paypalPayment = new PaypalPayment(null,"email1@gmail.com","email2@gmail.com",2000);
+        paypalTable.InsertToTable(paypalPayment);
+
+
+
+        UserTable.getInstance();
+        VacationTable.getInstance();
+        PurchaseTable.getInstance();
+        RequestTable.getInstance() ;
 
         UserTable userTable = UserTable.getInstance();
         VacationTable vacationTable = VacationTable.getInstance();
         PurchaseTable purchaseTable = PurchaseTable.getInstance();
+
+
         RequestTable requestTable = RequestTable.getInstance();
         UserModel userModel = new UserModel();
         RequestModel requestModel = new RequestModel();
         VacationModel vacationModel = new VacationModel();
         User user1 = new User("user1","p","p","p","p",19920101);
         User user2 = new User("user2","p","p","p","p",19920101);
+        User user3 = new User("user3","p","p","p","p",19920101);
         userTable.InsertToTable(user1);
         userTable.InsertToTable(user2);
-        Vacation vacation1 = new Vacation(null,"user1","TLV", "NYC",true,20181025,200);
-        Vacation vacation2 = new Vacation(null,"user1","TLV", "LAS",true,20181025,200);
-        String key = vacation2.getVacationKey();
-
+        userTable.InsertToTable(user3);
+//        Vacation vacation1 = new Vacation(null,"user1","TLV", "NYC",true,20181025,200);
+//        Vacation vacation2 = new Vacation(null,"user1","TLV", "LAS",true,20181025,200);
+//        String key = vacation2.getVacationKey();
 //        Request request1 = new Request(key,"user1","user2",false,20181025,Enum_RequestState.PENDING);
 //        requestModel.createNewData(request1);
 //        vacationModel.createNewData(vacation1);
